4475e96c7c43bbe62bfe08eb976d8b973bfc319f
Poglia <pedro.poglia@hotmail.com>
2022-02-19 16:29:31 -0300

    1.2.1 | Loop in arrays

---
+++ avancando/listas.php
@@ -0,0 +1,4 @@
+<?php
+  $idadeList = [21, 32, 12, 16, 70, 41, 13];
+
+  $primeiraIdade = $idadeList[0];
---
+++ avancando/loop-lista.php
@@ -0,0 +1,7 @@
+<?php
+  $idadeList = [21, 32, 12, 16, 70, 41, 13];
+
+  for ($i = 0; $i < 7; $i++)
+  {
+    echo $idadeList[$i] . PHP_EOL;
+  }
