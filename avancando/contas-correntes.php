5aa83c2f3d62b4244bfc9f7d35c4d32c6243856a
Poglia <pedro.poglia@hotmail.com>
2022-02-20 22:57:16 -0300

    1.2.2 | Associative Arrays

---
+++ avancando/contas-correntes.php
@@ -0,0 +1,14 @@
+<?php
+  $conta1 = ["titutar" => 'Poglia',
+             "saldo"   => 1000
+  ];
+
+  $conta2 = ["titutar" => 'Maria',
+             "saldo"   => 10000
+  ];
+
+  $conta3 = ["titutar" => 'Alberto',
+             "saldo"   => 300
+  ];
+
+  $contasCorrentes = [$conta1, $conta2, $conta3];
